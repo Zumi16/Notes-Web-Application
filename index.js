1a4dd066127be890e51275041959d64edd35258d
Zumi <mizuzumi16@gmail.com>
2025-11-02 19:25:16 +0800

    Modified ui for index file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-    
-    /* const dialog = document.getElementById('noteDialog');
+/* const dialog = document.getElementById('noteDialog');
     const openButton = document.getElementById('open-btn')
 
     openButton.addEventListener('click', () => {
         dialog.showModal();
     }); */
-    
