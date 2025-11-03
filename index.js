1126e13f7183b56181c2784e44c35177f0450165
Zumi <mizuzumi16@gmail.com>
2025-11-03 18:18:15 +0800

    Added javascript for functionalities

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const notes = [];
 const dialog = document.getElementById('noteDialog');
 const openNotesDialog = document.getElementById('addNotesBtn')
 
-    openNotesDialog.addEventListener('click', () => {
-        dialog.showModal();
-    });
+openNotesDialog.addEventListener('click', () => {
+    dialog.showModal();
+    titleInput.focus();
+});
 
 function closeNoteDialog() {
     dialog.close();
 }
+
+function saveNote(event) {
+    event.preventDefault();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('noteForm').addEventListener('submit', saveNote);
+
+
+    document.getElementById('noteDialog').addEventListener('click', function(event) {
+        if (event.target === this) {
+            closeNoteDialog();
+        };
+    });
+});
+
 
 /*
 function addNewNotes() {
