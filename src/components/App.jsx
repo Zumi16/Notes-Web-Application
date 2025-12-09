685b39e2cf6f2472f7f545e756f79357e1b0c3da
Zumi <mizuzumi16@gmail.com>
2025-12-09 21:44:08 +0800

    Added header content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import '../styles/App.css'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
     return (
     <>
-        <header class="flex p-5 px-15 border-b-3 justify-between h-17 items-center bg-[#FFFF85]">
-            <h1 class="text-[25px] font-medium">Notes Web Application</h1>
-            <div class="flex font-medium gap-4">
-                <button id="addNotesBtn" class="max-h-11 border-2 bg-white rounded-lg p-2 cursor-pointer" 
+        <header className="flex p-5 px-15 border-b-3 justify-between h-17 items-center bg-[#FFFF85]">
+            <h1 className="text-[25px] font-medium">Notes Web Application</h1>
+            <div className="flex font-medium gap-4">
+                <button id="addNotesBtn" className="max-h-11 border-2 bg-white rounded-lg p-2 cursor-pointer" 
                 >✚ Add Note
                 </button>
-                <button class="material-symbols-outlined bg-white rounded-lg p-2 cursor-pointer border-2"
+                <button className="material-symbols-outlined bg-white rounded-lg p-2 cursor-pointer border-2"
                 id="themeToggleBtn"
                 >Light
                 </button>
